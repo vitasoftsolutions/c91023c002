dfa694ccb92a46990caf4f450fdcb0ba55f18931
m-sabid <sabidofficial@gmail.com>
2023-09-24 20:36:58 +0600

    Added import code.

--- src/Components/shared/Modals/ImportModal.jsx
+++ src/Components/shared/Modals/ImportModal.jsx
@@ -1,9 +1,49 @@
 import { AiOutlineCloudDownload } from "react-icons/ai";
+import { useState } from "react"; // Import useState from React
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { base_url } from "../Url";
 
 const ImportModal = ({ importModal, onClose }) => {
+  const { register, handleSubmit } = useForm();
+  const [fileName, setFileName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+      setSelectedFile(file); // Store the selected file
+    }
+  };
+
   if (!importModal) {
     return null;
   }
+
+  const onSubmit = () => {
+    const submittedData = {
+      model_name: "LoanBeneficaries",
+      app_label: "loan",
+      file: selectedFile,
+    };
+    console.log(submittedData, "selectedFileselectedFile")
+    const token = sessionStorage.getItem("jwt_token");
+
+    // Define the headers
+    const headers = {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    };
+
+    // Make the Axios PUT request with the headers and payload
+    const response = axios.post(`${base_url}/upload-csv/`, submittedData, {
+      headers,
+    });
+
+    console.log(response);
+  };
+
   return (
     <dialog id="my_modal_1" className="modal bg-gray-800 bg-opacity-50" open>
       <div className="modal-box w-11/12 max-w-5xl rounded-none">
@@ -24,23 +64,52 @@ const ImportModal = ({ importModal, onClose }) => {
               </p>
             </div>
             {/* File 1 */}
-            <div>
-              <label className="border-2 border-erp_success border-dashed text-erp_dark text-center w-full py-5 px-4 inline-flex flex-col justify-center items-center cursor-pointer">
-                <span className="text-3xl">
-                  <AiOutlineCloudDownload />
-                </span>
-                <p className="font-bold">Drop files here or click to upload.</p>
-                <input type="file" className="hidden" accept=".csv" />
-              </label>
-              <div className="flex gap-2 justify-center my-3">
-                <button className="bg-[#5AC192] py-2 px-3 text-erp_light">
-                  Merge and Upload as one file
-                </button>
-                <button className="bg-[#5AC192] py-2 px-3 text-erp_light">
-                  Save each file separately
-                </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <label
+                  className="border-2 border-erp_success border-dashed text-erp_dark text-center w-full py-5 px-4 inline-flex flex-col justify-center items-center cursor-pointer"
+                  htmlFor="fileInput" // Add htmlFor attribute to link label and input
+                >
+                  <span className="text-3xl">
+                    <AiOutlineCloudDownload />
+                  </span>
+                  <p className="font-bold">
+                    {fileName
+                      ? fileName
+                      : "Drop files here or click to upload."}
+                  </p>
+                  {/* Use standard file input element */}
+                  <input
+                    {...register("fileInput")}
+                    type="file"
+                    accept=".csv"
+                    id="fileInput"
+                    className="hidden"
+                    onChange={handleFileChange}
+                  />
+                </label>
+                <input
+                  type="hidden"
+                  {...register("hiddenFileInput")}
+                  value={selectedFile ? selectedFile.name : ""}
+                />
+                <div className="flex gap-2 justify-center my-3">
+                  <button
+                    disabled
+                    className="bg-[#5AC192] hover:bg-[#449973] duration-300 py-2 px-3 text-erp_light"
+                  >
+                    Merge and Upload as one file
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-[#5AC192] hover:bg-[#449973] duration-300 py-2 px-3 text-erp_light"
+                  >
+                    Save each file separately
+                  </button>
+                </div>
               </div>
-            </div>
+            </form>
+            {/* form */}
           </div>
           <div className="md:col-span-3 col-span-1 bg-gray-100 min-h-[50vh]"></div>
         </div>
