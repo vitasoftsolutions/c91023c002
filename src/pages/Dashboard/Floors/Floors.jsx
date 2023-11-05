4050e315426ea7dd9250407637f4924ed44a6fa8
m-sabid <sabidofficial@gmail.com>
2023-11-05 22:39:19 +0700

    added Floors and make a conditional design

--- src/pages/Dashboard/Floors/Floors.jsx
+++ src/pages/Dashboard/Floors/Floors.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function FlatAndFloor() {
+const floors = [
+  { info: "in the name of pending", status: "pending" },
+  { info: "in the name of sold", status: "sold" },
+  { info: "in the name of pending", status: "pending" },
+  { info: "in the name of sold", status: "sold" },
+  { info: "in the name of under", status: "under" },
+];
+
+function Floors() {
   return (
-    <div>FlatAndFloor</div>
-  )
+    <div className="bg-white shadow-lg shadow-blue-200 md:mx-10 mb-5 rounded-lg md:p-4">
+      <div className="w-full mx-auto p-4 grid grid-cols-4 gap-x-4 rounded-md bg-opacity-50 backdrop-blur-md bg-gray-200">
+        {floors.map((dt) => {
+          return (
+            <div
+              key={dt}
+              className={`${
+                dt.status === "pending"
+                  ? "bg-blue-400"
+                  : dt.status === "sold"
+                  ? "bg-green-600"
+                  : dt.status === "under"
+                  ? "bg-red-400"
+                  : ""
+              }
+            p-3 rounded-sm h-28 mb-4`}
+            >
+              {dt.info}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default FlatAndFloor
+export default Floors;
