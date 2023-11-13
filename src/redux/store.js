2cc7188b2c5ea2a70c2ed89808a6a5396394673d
m-sabid <sabidofficial@gmail.com>
2023-11-13 23:35:16 +0700

    setup the store for [D[D[D[D[D[D[D[D[D[Dredux [D store for business profile

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import loanBeneListReducer from "./slices/searchLoanSlice";
 import loanBeneficiary from "./slices/loanBeneficiarySlice";
+import businessProfileReducer from "./slices/BusinessProfileSlice";
 import phoneReducers from "./slices/phoneSlice";
 import employeeReducers from "./slices/employeeSlice";
 import ownerReducers from "./slices/ownerBeneficiarySlice";
@@ -11,17 +11,19 @@ import typesReducers from "./slices/TypesSlice";
 import workprogressReducers from "./slices/WorkprogressSlice";
 import ContractorBenReducers from "./slices/ContractorBenSlice";
 import assignContractorSlice from "./slices/AssignContractorSlice";
+
+
+
 export const store = configureStore({
   reducer: {
-    // loanBeneList: loanBeneListReducer,
     loanBeneficiary: loanBeneficiary,
+    businessProfileReducer: businessProfileReducer,
     projectsReducer: projectsReducer,
     phoneReducers: phoneReducers,
     employeeReducers: employeeReducers,
     ownerReducers: ownerReducers,
     attendanceReducers: attendanceReducers,
     applabelReducer: applabelReducer,
-
     typesReducers: typesReducers,
     workprogressReducers:workprogressReducers,
     ContractorBenReducers:ContractorBenReducers,
