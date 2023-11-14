f2eedce80cdbac57cafe0ffcb0c7e38cf9c7bb9d
m-sabid <sabidofficial@gmail.com>
2023-11-14 11:08:17 +0700

    design project details page and a packege called react tabs

--- src/pages/Dashboard/Projects/ProjectsDetails.jsx
+++ src/pages/Dashboard/Projects/ProjectsDetails.jsx
@@ -1,18 +1,21 @@
 import { useEffect } from "react";
 import { BsArrowLeftShort } from "react-icons/bs";
 import { FaBuilding } from "react-icons/fa";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { IoLocation } from "react-icons/io5";
 import { Link, useLocation } from "react-router-dom";
 import Breadcrumb from "../../../Components/shared/Breadcrumb/Breadcrumb";
 import NoData from "../../../Components/shared/NoData";
 import { fetchFloors } from "../../../redux/Actions/FloorsAction";
+// Tabs
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function ProjectsDetails() {
   const dispatch = useDispatch();
-  const floorState = useSelector((state) =>
-    state.floorsReducer?.data?.slice().reverse()
-  );
   let { state } = useLocation();
+
+  console.log(state, "state");
 
   useEffect(() => {
     dispatch(fetchFloors(state?.id));
@@ -36,10 +39,59 @@ function ProjectsDetails() {
       ) : (
         <div className="bg-white shadow-lg shadow-blue-200 md:mx-8 mb-5 mt-5 rounded-lg md:p-4">
           <div
-            className={`w-full mx-auto p-4 grid bg-gray-200 gap-x-4 rounded-md bg-opacity-50 backdrop-blur-md bg-gray-200 min-h-[60vh] relative`}
+            className={`w-full mx-auto p-4 bg-gray-200 gap-x-4 rounded-md bg-opacity-50 backdrop-blur-md bg-gray-200 min-h-[60vh] relative`}
           >
+            <div>
+              <h3 className="text-erp_primary font-bold text-2xl">
+                {state?.name}
+              </h3>
+              <div className="flex items-center gap-2">
+                <p className="text-erp_danger">
+                  <IoLocation />
+                </p>
+                <p>{state.division},</p>
+                <p>{state.police_station},</p>
+                <p>{state.city}</p>
+              </div>
+            </div>
 
-            
+            <div className="flex gap-8 justify-between items-start mb-5 mt-8">
+              <div className="h-20 w-1/2 bg-gray-300 rounded-md p-3">
+                <p className="text-xl font-semibold text-gray-800">
+                  Total Earning
+                </p>
+                <p className="text-xl font-semibold text-gray-800 ml-1">0000</p>
+              </div>
+              <div className="h-20 w-1/2 bg-gray-300 rounded-md p-3">
+                <p className="text-xl font-semibold text-gray-800">
+                  Total Expense
+                </p>
+                <p className="text-xl font-semibold text-gray-800 ml-1">0000</p>
+              </div>
+            </div>
+
+            <div className="">
+              <p className="font-semibold text-xl border-white border-b-2 mb-2">
+                Bills
+              </p>
+              <Tabs>
+                <TabList className="">
+                  <Tab>Contactor</Tab>
+                  <Tab>Suppliers</Tab>
+                  <Tab>Bills</Tab>
+                </TabList>
+
+                <TabPanel className="my-3">
+                  <h2>Content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                  <h2>Content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                  <h2>Content 3</h2>
+                </TabPanel>
+              </Tabs>
+            </div>
 
             <Link
               to={`/projects/${state.id}/floor-details`}
