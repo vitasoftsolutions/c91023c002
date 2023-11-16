e991590878bd66f3fc70b2971ec39be816fd5604
m-sabid <sabidofficial@gmail.com>
2023-11-16 11:36:06 +0600

    Create salary folder with Create update and list pages

---
+++ src/pages/Dashboard/Salaraies/Salary.jsx
@@ -0,0 +1,107 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TableHeader from "../../../Components/shared/TableHeader/TableHeader";
+import GlobalTable from "../../../Components/shared/Tables/GlobalTable";
+import Swal from "sweetalert2";
+import {
+  deleteLoanBeneficiary,
+  fetchLoanBeneList,
+} from "../../../redux/Actions/loanBenAction";
+
+const t_head = [
+  { name: "Name" },
+  { name: "Image" },
+  { name: "Join Date" },
+  { name: "E-mail" },
+  { name: "Status" },
+  { name: "Actions" },
+];
+
+const Salary = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.loanBeneficiary);
+  // allDataList
+  const allDataList = state.data;
+  const newData = state?.data?.map((item) => ({
+    id: item.id,
+    first_name: item.first_name + " " + item.last_name,
+    image: item.profile_picture,
+    date: item.created_at,
+    email: item.email,
+    status: item.status,
+  }));
+  const tableData = {
+    ...state,
+    data: newData,
+  };
+  //
+
+  // console.log(tableData, "tableData");
+
+  const current_page = state.currentPage;
+  const total_page = state.totalPages;
+
+  // Pages
+  let page_number = [];
+  for (let i = current_page - 1; i <= current_page + 1; i++) {
+    if (i < 1) continue;
+    if (i > total_page) break;
+    page_number.push(i);
+  }
+
+  useEffect(() => {
+    dispatch(fetchLoanBeneList(current_page));
+  }, [dispatch, current_page, state.isDelete, state.isUpdate]);
+
+  const handlePageChange = (newPage) => {
+    dispatch(fetchLoanBeneList(newPage));
+  };
+
+  // console.log(state, "state_ page");
+
+  const deleteFunction = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteLoanBeneficiary(id));
+        if (state.isDelete === true) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        }
+      }
+    });
+  };
+
+  //
+  //
+  return (
+    <div className="max-w-screen">
+      <TableHeader
+        title={"Beneficiary"}
+        redirectLink={"/beneficiarylist/loan-beneficiary-crete"}
+        url_endpoint={"/export-csv/?model=LoanBeneficaries&app_label=loan"}
+      />
+      <GlobalTable
+        t_head={t_head}
+        t_data={tableData}
+        allDataList={allDataList}
+        handlePageChange={handlePageChange}
+        current_page={current_page}
+        page_number={page_number}
+        deleteFunction={deleteFunction}
+        editLink={"/beneficiarylist/editloan"}
+        erp_modalCol={6}
+        photoSection={true}
+        nidSection={true}
+      />
+    </div>
+  );
+};
+
+export default Salary;
